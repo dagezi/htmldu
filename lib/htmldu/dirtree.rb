113b0b4c8cbf5328142389a2304e0c6a2d5d2e4c
Takeshi SASAKI <dagezi@gmail.com>
2016-01-02 09:00:30 +0900

    Found `@block_size` is not needed.

--- lib/htmldu/dirtree.rb
+++ lib/htmldu/dirtree.rb
@@ -2,8 +2,10 @@ require 'json'
 
 module Htmldu
   class Dirtree
-    attr_reader :path, :name, :root, :blocks, :children, :occupying_size, :block_size
+    attr_reader :path, :name, :root, :blocks, :children
          
+    BLOCK_SIZE=512
+
     def initialize(root, name = nil)
       @root = root
       @name = name
@@ -11,10 +13,7 @@ module Htmldu
     end
 
     def generateTree()
-      stat = File.lstat(@path)
-      @block_size = stat.blksize
-      blocks = stat.blocks
-
+      blocks = 0
       @children = []
       Dir.foreach(@path) do |name|
         next if name == '.' or name == '..'
@@ -30,15 +29,12 @@ module Htmldu
         end
       end
       @blocks = blocks
-      @occupying_size = blocks * @block_size
     end
 
     def toJson(json: '')
       json << "{\"name\":\"#{@name}\","
       json << "\"path\":\"#{@path}\","
       json << "\"blocks\":#{@blocks},"
-      json << "\"block_size\":#{@block_size},"
-      json << "\"occupying_size\":#{@occupying_size},"
       json << '"children":['
       c = ''
       @children.each { |dir|
