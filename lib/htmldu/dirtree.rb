06a4a28abf2527b8d8d7d4de427f3015aebb0018
Takeshi SASAKI <dagezi@gmail.com>
2016-01-31 20:54:09 +0900

    Don't stop on permission or other errors.

--- lib/htmldu/dirtree.rb
+++ lib/htmldu/dirtree.rb
@@ -2,7 +2,7 @@ require 'json'
 
 module Htmldu
   class Dirtree
-    attr_reader :path, :name, :root, :blocks, :children
+    attr_reader :path, :name, :root, :blocks, :children, :accurate
          
     BLOCK_SIZE=512
 
@@ -14,27 +14,35 @@ module Htmldu
 
     def generate_tree()
       blocks = 0
+      accurate = true
       @children = []
       Dir.foreach(@path) do |name|
         next if name == '.' or name == '..'
         path = File.join(@path, name)
-        stat = File.lstat(path)
-        blocks += stat.blocks
+        begin
+          stat = File.lstat(path)
+          blocks += stat.blocks
 
-        if stat.directory? and !stat.symlink?
-          child = Dirtree.new(@path, name)
-          child.generate_tree
-          @children << child
-          blocks += child.blocks
+          if stat.directory? and !stat.symlink?
+            child = Dirtree.new(@path, name)
+            child.generate_tree
+            @children << child
+            blocks += child.blocks
+            accurate &&= child.accurate
+          end
+        rescue => e
+          accurate = false
         end
       end
       @blocks = blocks
+      @accurate = accurate
     end
 
     def to_json(json: '')
       json << "{\"name\":\"#{@name}\","
       json << "\"path\":\"#{@path}\","
       json << "\"blocks\":#{@blocks},"
+      json << "\"accurate\":#{@accurate},"
       json << '"children":['
       c = ''
       @children.each { |dir|
